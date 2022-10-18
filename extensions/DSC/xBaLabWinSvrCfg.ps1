894e28e6570317ffdbff4b6e6da7693c46179946
Chris Langford <chris.langford@lang-ford.uk>
2022-10-18 14:02:59 +0100

    added new course

--- extensions/DSC/xBaLabWinSvrCfg.ps1
+++ extensions/DSC/xBaLabWinSvrCfg.ps1
@@ -1,7 +1,7 @@
 ################################################################
 # Script to configure Windows lab environment using DSC        #
 # Author: Chris Langford                                       #
-# Version: 2.6.0                                               #
+# Version: 2.7.0                                               #
 ################################################################
 
 Configuration xBaMobilityandDevicesLabCfg {
@@ -806,6 +806,117 @@ Configuration xBaSuppItArchP2LabCfg {
                 New-VM -Name "CallumPC" -MemoryStartupBytes 1GB -Generation 1 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\SuppItArchP2\CallumPC.vhdx" -SwitchName "vSwitch"
                 New-VM -Name "KiaraLaptop" -MemoryStartupBytes 1GB -Generation 1 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\SuppItArchP2\KiaraLaptop.vhdx" -SwitchName "vSwitch"
                 New-VM -Name "MariaPC" -MemoryStartupBytes 1GB -Generation 1 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\SuppItArchP2\MariaPC.vhdx" -SwitchName "vSwitch"
+            }
+            TestScript = { $false }
+            GetScript = {  
+                # Do Nothing
+            }
+            DependsOn = "[xWindowsFeatureSet]AddHyperVFeatures"
+        }
+
+        # This resource block ensures that the file or command is executed
+        xScript "SetRdpTimeZone"
+        {
+            SetScript = {
+                New-ItemProperty -ErrorAction SilentlyContinue -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Name "fEnableTimeZoneRedirection" -Value "1" -PropertyType DWORD -Force
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+        }
+    }
+    
+}
+
+Configuration xBaTrblshootNetP2LabCfg {
+    [CmdletBinding()]
+
+    Param (
+        
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential
+    )
+
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+    $features = @("Hyper-V", "RSAT-Hyper-V-Tools", "Hyper-V-Tools", "Hyper-V-PowerShell")
+
+    Node localhost {
+
+        LocalConfigurationManager {
+            RebootNodeIfNeeded = $true
+        }
+
+        # This resource block create a local user
+        xUser "CreateUserAccount" {
+            Ensure = "Present"
+            Username = Split-Path -Path $Credential.Username -Leaf
+            Password = $Credential
+            FullName = "Baltic Apprentice"
+            Description = "Baltic Apprentice"
+            PasswordNeverExpires = $true
+            PasswordChangeRequired = $false
+            PasswordChangeNotAllowed = $true
+        }
+
+        # This resource block adds user to a spacific group
+        xGroup "AddToRemoteDesktopUserGroup"
+        {
+            GroupName = "Remote Desktop Users"
+            Ensure = "Present"
+            MembersToInclude = Split-Path -Path $Credential.Username -Leaf
+            DependsOn = "[xUser]CreateUserAccount"
+        }
+
+        # This resource block adds user to a spacific group
+        xGroup "AddToHyperVAdministratorGroup"
+        {
+            GroupName = "Hyper-V Administrators"
+            Ensure = "Present"
+            MembersToInclude = Split-Path -Path $Credential.Username -Leaf
+            DependsOn = "[xUser]CreateUserAccount"
+        }
+
+        # This resource block ensures that a Windows Features (Roles) is present
+        xWindowsFeatureSet "AddHyperVFeatures"
+        {
+            Name = $features
+            Ensure = "Present"
+            IncludeAllSubFeature = $true
+        }
+
+        # This resource block ensures that the file is executed
+        xScript "RunvSwitchForNestedVms"
+        {
+            SetScript = { 
+                New-VMSwitch -SwitchName "Int-vSwitch" -SwitchType Internal
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+            DependsOn = "[xWindowsFeatureSet]AddHyperVFeatures"
+        }
+
+        xScript "RunCreateVms"
+        {
+            SetScript = { 
+                New-VM -Name "Problem 1 Server" -MemoryStartupBytes 2GB -Generation 2 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\TroubleshootNetworks\Server01-p1.vhdx" -SwitchName "Int-vSwitch"
+                New-VM -Name "Problem 1 Client" -MemoryStartupBytes 1GB -Generation 1 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\TroubleshootNetworks\Client01-p1.vhdx" -SwitchName "Int-vSwitch"
+                New-VM -Name "Problem 2 Server" -MemoryStartupBytes 2GB -Generation 2 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\TroubleshootNetworks\Server01-p2.vhdx" -SwitchName "Int-vSwitch"
+                New-VM -Name "Problem 2 Client" -MemoryStartupBytes 1GB -Generation 1 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\TroubleshootNetworks\Client01-p2.vhdx" -SwitchName "Int-vSwitch"
+                New-VM -Name "Problem 3 Server" -MemoryStartupBytes 2GB -Generation 2 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\TroubleshootNetworks\Server01-p3.vhdx" -SwitchName "Int-vSwitch"
+                New-VM -Name "Problem 3 Client" -MemoryStartupBytes 1GB -Generation 1 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\TroubleshootNetworks\Client01-p3.vhdx" -SwitchName "Int-vSwitch"
+                New-VM -Name "Problem 4 Server" -MemoryStartupBytes 2GB -Generation 2 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\TroubleshootNetworks\Server01-p4.vhdx" -SwitchName "Int-vSwitch"
+                New-VM -Name "Problem 4 Client" -MemoryStartupBytes 1GB -Generation 1 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\TroubleshootNetworks\Client01-p4.vhdx" -SwitchName "Int-vSwitch"
+                New-VM -Name "Problem 5 Server" -MemoryStartupBytes 2GB -Generation 2 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\TroubleshootNetworks\Server01-p5.vhdx" -SwitchName "Int-vSwitch"
+                New-VM -Name "Problem 5 Client" -MemoryStartupBytes 1GB -Generation 1 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\TroubleshootNetworks\Client01-p5.vhdx" -SwitchName "Int-vSwitch"
+                New-VM -Name "Problem 6 Server" -MemoryStartupBytes 2GB -Generation 2 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\TroubleshootNetworks\Server01-p6.vhdx" -SwitchName "Int-vSwitch"
+                New-VM -Name "Problem 6 Client" -MemoryStartupBytes 1GB -Generation 1 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\TroubleshootNetworks\Client01-p6.vhdx" -SwitchName "Int-vSwitch"
+                
             }
             TestScript = { $false }
             GetScript = {  
