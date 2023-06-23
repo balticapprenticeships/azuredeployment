7322f831c8ad16c36f1d731ba3a0f3c0a34fb680
Chris Langford <chris.langford@lang-ford.uk>
2023-06-23 16:23:25 +0100

    added v4.9.0

--- extensions/DSC/xBaLabWinSvrCfg.ps1
+++ extensions/DSC/xBaLabWinSvrCfg.ps1
@@ -1,7 +1,7 @@
 ################################################################
 # Script to configure Windows lab environment using DSC        #
 # Author: Chris Langford                                       #
-# Version: 4.8.0                                               #
+# Version: 4.9.0                                               #
 ################################################################
 
 Configuration xBaICTSupC1LabCfg {
@@ -1479,6 +1479,83 @@ Configuration xBaL4NetEngC4LabCfg {
 }
 
 Configuration xBaL4NetEngC5LabCfg {
+    [CmdletBinding()]
+
+    Param (
+        
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential
+    )
+
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+    $features = @("Hyper-V", "RSAT-Hyper-V-Tools", "Hyper-V-Tools", "Hyper-V-PowerShell")
+
+    Node localhost {
+
+        LocalConfigurationManager {
+            RebootNodeIfNeeded = $true
+        }
+
+        # This resource block create a local user
+        xUser "CreateUserAccount" {
+            Ensure = "Present"
+            Username = Split-Path -Path $Credential.Username -Leaf
+            Password = $Credential
+            FullName = "Baltic Apprentice"
+            Description = "Baltic Apprentice"
+            PasswordNeverExpires = $true
+            PasswordChangeRequired = $false
+            PasswordChangeNotAllowed = $true
+        }
+
+        # This resource block adds user to a spacific group
+        xGroup "AddToRemoteDesktopUserGroup"
+        {
+            GroupName = "Remote Desktop Users"
+            Ensure = "Present"
+            MembersToInclude = Split-Path -Path $Credential.Username -Leaf
+            DependsOn = "[xUser]CreateUserAccount"
+        }
+
+        # This resource block adds user to a spacific group
+        xGroup "AddToHyperVAdministratorGroup"
+        {
+            GroupName = "Hyper-V Administrators"
+            Ensure = "Present"
+            MembersToInclude = Split-Path -Path $Credential.Username -Leaf
+            DependsOn = "[xUser]CreateUserAccount"
+        }
+
+        # This resource block ensures that a Windows Features (Roles) is present
+        xWindowsFeatureSet "AddHyperVFeatures"
+        {
+            Name = $features
+            Ensure = "Present"
+            IncludeAllSubFeature = $true
+        }
+
+        # This resource block ensures that the file or command is executed
+        xScript "RemoveArtifacts"
+        {
+            SetScript = {
+                Remove-Item "C:\workflow-artifacts\*" -Recurse -Force
+                Remove-Item "C:\workflow-artifacts" -Force
+                Remove-Item "C:\workflow-artifacts.zip" -Force
+                Remove-Item "C:\*_buildlog.log" -Force
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+        }
+    }
+    
+}
+
+Configuration xBaL4NetEngC7LabCfg {
     [CmdletBinding()]
 
     Param (
