969b2e567a48847cd7eb94aa2cb0f6b471f9042a
Chris Langford <105057902+supercoach91@users.noreply.github.com>
2023-12-01 12:47:09 +0000

    added ICTSupC3N & ICTSup6N

--- extensions/DSC/xBaLabWinSvrCfg.ps1
+++ extensions/DSC/xBaLabWinSvrCfg.ps1
@@ -894,6 +894,266 @@ Configuration xBaICTSupC6LabCfg {
     
 }
 
+Configuration xBaICTSuoC3NLabCfg {
+    [CmdletBinding()]
+
+    Param (
+        
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential
+    )
+
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+    $features = @("Hyper-V", "RSAT-Hyper-V-Tools", "Hyper-V-Tools", "Hyper-V-PowerShell")
+
+    Node localhost {
+
+        LocalConfigurationManager {
+            RebootNodeIfNeeded = $true
+        }
+
+        # This resource block create a local user
+        xUser "CreateUserAccount" {
+            Ensure = "Present"
+            Username = Split-Path -Path $Credential.Username -Leaf
+            Password = $Credential
+            FullName = "Baltic Apprentice"
+            Description = "Baltic Apprentice"
+            PasswordNeverExpires = $true
+            PasswordChangeRequired = $false
+            PasswordChangeNotAllowed = $true
+        }
+
+        # This resource block adds user to a spacific group
+        xGroup "AddToRemoteDesktopUserGroup"
+        {
+            GroupName = "Remote Desktop Users"
+            Ensure = "Present"
+            MembersToInclude = Split-Path -Path $Credential.Username -Leaf
+            DependsOn = "[xUser]CreateUserAccount"
+        }
+
+        # This resource block adds user to a spacific group
+        xGroup "AddToHyperVAdministratorGroup"
+        {
+            GroupName = "Hyper-V Administrators"
+            Ensure = "Present"
+            MembersToInclude = Split-Path -Path $Credential.Username -Leaf
+            DependsOn = "[xUser]CreateUserAccount"
+        }
+
+        # This resource block ensures that a Windows Features (Roles) is present
+        xWindowsFeatureSet "AddHyperVFeatures"
+        {
+            Name = $features
+            Ensure = "Present"
+            IncludeAllSubFeature = $true
+        }
+
+        # This resource block ensures that the file is executed
+        xScript "SetDefaultVirtualHardDiskLocation"
+        {
+            SetScript = { 
+                Set-VMHost -VirtualHardDiskPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks"
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+            DependsOn = "[xWindowsFeatureSet]AddHyperVFeatures"
+        }
+
+        # This resource block ensures that the file is executed
+        xScript "RunvSwitchForNestedVms"
+        {
+            SetScript = { 
+                New-VMSwitch -SwitchName "Int-vSwitch" -SwitchType Private
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+            DependsOn = "[xWindowsFeatureSet]AddHyperVFeatures"
+        }
+
+        # This resource block ensures that the file is executed
+        xScript "RunvSwitchForNat"
+        {
+            SetScript = { 
+                New-VMSwitch -SwitchName "Int-vSwitch" -SwitchType Internal
+                New-NetIPAddress -InterfaceAlias "vEthernet (Int-vSwitch)" -IPAddress 172.16.20.254 -PrefixLength 24
+                New-NetNat -Name "Nat-VM" -InternalIPInterfaceAddressPrefix 172.16.20.0/24
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+            DependsOn = "[xWindowsFeatureSet]AddHyperVFeatures"
+        }
+
+        # This resource block ensures that the file or command is executed
+        xScript "SetRdpTimeZone"
+        {
+            SetScript = {
+                New-ItemProperty -ErrorAction SilentlyContinue -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Name "fEnableTimeZoneRedirection" -Value "1" -PropertyType DWORD -Force
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+        }
+
+        # This resource block ensures that the file or command is executed
+        xScript "RemoveArtifacts"
+        {
+            SetScript = {
+                Remove-Item "C:\workflow-artifacts\*" -Recurse -Force
+                Remove-Item "C:\workflow-artifacts" -Force
+                Remove-Item "C:\workflow-artifacts.zip" -Force
+                Remove-Item "C:\*_buildlog.log" -Force
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+        }
+    }
+    
+}
+
+Configuration xBaICTSupC6NLabCfg {
+    [CmdletBinding()]
+
+    Param (
+        
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential
+    )
+
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+    $features = @("Hyper-V", "RSAT-Hyper-V-Tools", "Hyper-V-Tools", "Hyper-V-PowerShell")
+
+    Node localhost {
+
+        LocalConfigurationManager {
+            RebootNodeIfNeeded = $true
+        }
+
+        # This resource block create a local user
+        xUser "CreateUserAccount" {
+            Ensure = "Present"
+            Username = Split-Path -Path $Credential.Username -Leaf
+            Password = $Credential
+            FullName = "Baltic Apprentice"
+            Description = "Baltic Apprentice"
+            PasswordNeverExpires = $true
+            PasswordChangeRequired = $false
+            PasswordChangeNotAllowed = $true
+        }
+
+        # This resource block adds user to a spacific group
+        xGroup "AddToRemoteDesktopUserGroup"
+        {
+            GroupName = "Remote Desktop Users"
+            Ensure = "Present"
+            MembersToInclude = Split-Path -Path $Credential.Username -Leaf
+            DependsOn = "[xUser]CreateUserAccount"
+        }
+
+        # This resource block adds user to a spacific group
+        xGroup "AddToHyperVAdministratorGroup"
+        {
+            GroupName = "Hyper-V Administrators"
+            Ensure = "Present"
+            MembersToInclude = Split-Path -Path $Credential.Username -Leaf
+            DependsOn = "[xUser]CreateUserAccount"
+        }
+
+        # This resource block ensures that a Windows Features (Roles) is present
+        xWindowsFeatureSet "AddHyperVFeatures"
+        {
+            Name = $features
+            Ensure = "Present"
+            IncludeAllSubFeature = $true
+        }
+
+        # This resource block ensures that the file is executed
+        xScript "SetDefaultVirtualHardDiskLocation"
+        {
+            SetScript = { 
+                Set-VMHost -VirtualHardDiskPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks"
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+            DependsOn = "[xWindowsFeatureSet]AddHyperVFeatures"
+        }
+
+        # This resource block ensures that the file is executed
+        xScript "RunvSwitchForNestedVms"
+        {
+            SetScript = { 
+                New-VMSwitch -SwitchName "Int-vSwitch" -SwitchType Private
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+            DependsOn = "[xWindowsFeatureSet]AddHyperVFeatures"
+        }
+
+        # This resource block ensures that the file is executed
+        xScript "RunvSwitchForNat"
+        {
+            SetScript = { 
+                New-VMSwitch -SwitchName "Int-vSwitch" -SwitchType Internal
+                New-NetIPAddress -InterfaceAlias "vEthernet (Int-vSwitch)" -IPAddress 172.16.20.254 -PrefixLength 24
+                New-NetNat -Name "Nat-VM" -InternalIPInterfaceAddressPrefix 172.16.20.0/24
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+            DependsOn = "[xWindowsFeatureSet]AddHyperVFeatures"
+        }
+
+        # This resource block ensures that the file or command is executed
+        xScript "SetRdpTimeZone"
+        {
+            SetScript = {
+                New-ItemProperty -ErrorAction SilentlyContinue -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Name "fEnableTimeZoneRedirection" -Value "1" -PropertyType DWORD -Force
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+        }
+
+        # This resource block ensures that the file or command is executed
+        xScript "RemoveArtifacts"
+        {
+            SetScript = {
+                Remove-Item "C:\workflow-artifacts\*" -Recurse -Force
+                Remove-Item "C:\workflow-artifacts" -Force
+                Remove-Item "C:\workflow-artifacts.zip" -Force
+                Remove-Item "C:\*_buildlog.log" -Force
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+        }
+    }
+    
+}
+
 Configuration xBaICTSupMstrLabCfg {
     [CmdletBinding()]
 
