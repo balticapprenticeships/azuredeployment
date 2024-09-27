f70ff21844e750ad2e04774b73a035be7686237f
Chris Langford <chris.langford@lang-ford.uk>
2024-09-27 18:28:26 +0100

    Added new L4NetEngC4 lab config

--- extensions/DSC/xBaLabWinSvrCfg.ps1
+++ extensions/DSC/xBaLabWinSvrCfg.ps1
@@ -1,7 +1,7 @@
 ################################################################
 # Script to configure Windows lab environment using DSC        #
 # Author: Chris Langford                                       #
-# Version: 5.5.0                                               #
+# Version: 5.6.0                                               #
 ################################################################
 
 Configuration xBaICTSupC1LabCfg {
@@ -1278,7 +1278,7 @@ Configuration xBaICTSupMstrLabCfg {
     
 }
 
-Configuration xBaL4NetEngC4LabCfg {
+Configuration xBaL4NetEngC4OLDLabCfg {
     [CmdletBinding()]
 
     Param (
@@ -1355,6 +1355,156 @@ Configuration xBaL4NetEngC4LabCfg {
             SetScript = {
                 New-VM -Name "Task 2 Client" -MemoryStartupBytes 2GB -Generation 1 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\NetEngCourse4\Task2Client.vhdx" -SwitchName "vSwitch"
                 New-VM -Name "Task 2 Server" -MemoryStartupBytes 2GB -Generation 2 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\NetEngCourse4\Task2Server.vhdx" -SwitchName "vSwitch"
+                
+            }
+            TestScript = { $false }
+            GetScript = {  
+                # Do Nothing
+            }
+            DependsOn = "[xWindowsFeatureSet]AddHyperVFeatures"
+        }
+
+        # This resource block ensures that the file or command is executed
+        xScript "SetRdpTimeZone"
+        {
+            SetScript = {
+                New-ItemProperty -ErrorAction SilentlyContinue -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -Name "fEnableTimeZoneRedirection" -Value "1" -PropertyType DWORD -Force
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+        }
+
+        # This resource block ensures that the file or command is executed
+        xScript "RemoveArtifacts"
+        {
+            SetScript = {
+                Remove-Item "C:\workflow-artifacts\*" -Recurse -Force
+                Remove-Item "C:\workflow-artifacts" -Force
+                Remove-Item "C:\workflow-artifacts.zip" -Force
+                Remove-Item "C:\*_buildlog.log" -Force
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+        }
+    }
+    
+}
+
+Configuration xBaL4NetEngC4LabCfg {
+    [CmdletBinding()]
+
+    Param (
+        
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.Credential()]
+        $Credential
+    )
+
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+    $features = @("Hyper-V", "RSAT-Hyper-V-Tools", "Hyper-V-Tools", "Hyper-V-PowerShell")
+
+    Node localhost {
+
+        LocalConfigurationManager {
+            RebootNodeIfNeeded = $true
+        }
+
+        # This resource block create a local user
+        xUser "CreateUserAccount" {
+            Ensure = "Present"
+            Username = Split-Path -Path $Credential.Username -Leaf
+            Password = $Credential
+            FullName = "Baltic Apprentice"
+            Description = "Baltic Apprentice"
+            PasswordNeverExpires = $true
+            PasswordChangeRequired = $false
+            PasswordChangeNotAllowed = $true
+        }
+
+        # This resource block adds user to a spacific group
+        xGroup "AddToRemoteDesktopUserGroup"
+        {
+            GroupName = "Remote Desktop Users"
+            Ensure = "Present"
+            MembersToInclude = Split-Path -Path $Credential.Username -Leaf
+            DependsOn = "[xUser]CreateUserAccount"
+        }
+
+        # This resource block adds user to a spacific group
+        xGroup "AddToHyperVAdministratorGroup"
+        {
+            GroupName = "Hyper-V Administrators"
+            Ensure = "Present"
+            MembersToInclude = Split-Path -Path $Credential.Username -Leaf
+            DependsOn = "[xUser]CreateUserAccount"
+        }
+
+        # This resource block ensures that a Windows Features (Roles) is present
+        xWindowsFeatureSet "AddHyperVFeatures"
+        {
+            Name = $features
+            Ensure = "Present"
+            IncludeAllSubFeature = $true
+        }
+
+        # This resource block ensures that the file is executed
+        xScript "SetDefaultVirtualHardDiskLocation"
+        {
+            SetScript = { 
+                Set-VMHost -VirtualHardDiskPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks"
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+            DependsOn = "[xWindowsFeatureSet]AddHyperVFeatures"
+        }
+
+        # This resource block ensures that the file is executed
+        xScript "RunvSwitchForNat"
+        {
+            SetScript = { 
+                New-VMSwitch -SwitchName "WAN" -SwitchType Internal
+                New-NetIPAddress -InterfaceAlias "vEthernet (WAN)" -IPAddress 172.16.20.254 -PrefixLength 24
+                New-NetNat -Name "Nat-VM" -InternalIPInterfaceAddressPrefix 172.16.20.0/24
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+            DependsOn = "[xWindowsFeatureSet]AddHyperVFeatures"
+        }
+
+        # This resource block ensures that the file is executed
+        xScript "RunvSwitchForNestedVms"
+        {
+            SetScript = { 
+                New-VMSwitch -SwitchName "LAN" -SwitchType Private
+                New-VMSwitch -SwitchName "LAN2" -SwitchType Private
+
+            }
+            TestScript = { $false }
+            GetScript = { 
+                # Do Nothing
+            }
+            DependsOn = "[xWindowsFeatureSet]AddHyperVFeatures"
+        }
+
+        xScript "RunCreateVms"
+        {
+            SetScript = { 
+                New-VM -Name "pfSense" -MemoryStartupBytes 1GB -Generation 1 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\L4NetEngC4\pfsense.vhdx" -SwitchName "WAN"
+                Add-VMNetworkAdapter -VMName "pfSense" -SwitchName "LAN"
+                Add-VMNetworkAdapter -VMName "pfSense" -SwitchName "LAN2"
+                New-VM -Name "ManagementServer" -MemoryStartupBytes 2GB -Generation 2 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\L4NetEngC4\WinSvr22.vhdx" -SwitchName "LAN"
+                Add-VMHardDiskDrive -VMName "ManagementServer" -Path "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\L4NetEngC4\Data01.vhdx"
+                New-VM -Name "Client1" -MemoryStartupBytes 1GB -Generation 1 -BootDevice VHD -VHDPath "C:\Users\Public\Documents\Hyper-V\Virtual hard disks\L4NetEngC4\Win10-Client01.vhdx" -SwitchName "LAN"
                 
             }
             TestScript = { $false }
